03c3be07868fcf4c3d27e01048572a7604125ad4
Tamer Nasser <tamernasser@outlook.com>
2019-03-14 00:45:08 +0200

    Re-named variables

--- navBar/weather.js
+++ navBar/weather.js
@@ -1,10 +1,10 @@
-const URL = "http://api.openweathermap.org/data/2.5/weather?q=";
-const APIKEY = "&appid=9481079f5f89f0b72f0027d093f78ed5"
-var city = "Nazareth"
+const WEATHER_API = "http://api.openweathermap.org/data/2.5/weather?q=";
+const API_KEY = "&appid=9481079f5f89f0b72f0027d093f78ed5"
+const CITY_DEFAULT = "Nazareth"
 
 function getTemp(city) {
   var ele = document.getElementById("temp");
-  fetch(URL + city + APIKEY)
+  fetch(WEATHER_API + city + API_KEY)
     .then(function(response) {
       return response.json();
     })
@@ -15,4 +15,4 @@ function getTemp(city) {
     })
 }
 
-getTemp(city);
+getTemp(CITY_DEFAULT);

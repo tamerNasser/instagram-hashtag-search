a6b78f4bc216a5918ebefffe9b304a675810f5b7
Tamer Nasser <tamernasser@outlook.com>
2019-03-13 21:50:08 +0200

    Added post link on photo click related #19

--- instagram/apigram.js
+++ instagram/apigram.js
@@ -1,5 +1,6 @@
 const INSTAGRAM_API = 'https://www.instagram.com/explore/tags/';
 const INSTAGRAM_API_QUERY = '/?__a=1';
+const INSTAGRAM_POST_URL = 'https://www.instagram.com/p/';
 
 setPostsInfo('winter');
 
@@ -26,17 +27,19 @@ function searchLink(){
 
 function setPostsInfo(hashtag){
   let arrItems = Array.from(document.getElementsByClassName("postItem"));
+  let arrPosts;
   getData(INSTAGRAM_API+hashtag+INSTAGRAM_API_QUERY,function(data){
-    let arrPosts = data.graphql.hashtag.edge_hashtag_to_top_posts.edges;
+    arrPosts = data.graphql.hashtag.edge_hashtag_to_top_posts.edges;
     console.log(arrPosts);
     for(let i=0;i<arrItems.length;i++){
-      arrItems[i].childNodes[1].src = arrPosts[i].node.display_url;
+      arrItems[i].childNodes[1].firstChild.src = arrPosts[i].node.display_url;
+      arrItems[i].childNodes[1].href = INSTAGRAM_POST_URL+arrPosts[i].node.shortcode;
+      
     }
-
     // console.log(data.graphql.hashtag.edge_hashtag_to_top_posts.edges[0].node.edge_liked_by.count);
     // postsArr = data.graphql.hashtag;
-
   })
+
 }
 
 searchLink();

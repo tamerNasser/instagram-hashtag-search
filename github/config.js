9c764dbf6b67a42f1f24dffef36d62d59c6e8977
Tamer Nasser <tamernasser@outlook.com>
2019-03-13 14:17:49 +0200

    Added instagram api files and removed google

--- github/config.js
+++ github/config.js
@@ -1,6 +1,6 @@
 var config = {
   githubApi : 'https://api.github.com/users/',
-  accessToken : '?access_token=1b3cb85e9aa3dc7c51934062bf8033291b93cfb4',
+  accessToken : '?access_token=95dfa1ca8554b33c83b035c370fbe3813baa0938',
   codeWarsApi : 'https://www.codewars.com/api/v1/users/',
   codeWarsToken : '?access_key=s9zbYSGah3Jmy6ofmr6pC'
 };

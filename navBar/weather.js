7f5e7331bd41fff13cc3673b597aa80446c5daf9
Tamer Nasser <tamernasser@outlook.com>
2019-03-14 00:47:25 +0200

    Removed useless logs and replace var with let

--- navBar/weather.js
+++ navBar/weather.js
@@ -3,14 +3,13 @@ const API_KEY = "&appid=9481079f5f89f0b72f0027d093f78ed5"
 const CITY_DEFAULT = "Nazareth"
 
 function getTemp(city) {
-  var ele = document.getElementById("temp");
+  let ele = document.getElementById("temp");
   fetch(WEATHER_API + city + API_KEY)
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
       let temp = data.main.temp - 273.15;
-      console.log(temp);
       ele.innerText = Math.round(temp);
     })
     .catch(function(error){

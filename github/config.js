4af5d374bcb80c079c08837d274ede9ce482ebdb
majdya <majd.ya.1995@gmail.com>
2019-03-13 11:52:09 +0200

    added access token

--- github/config.js
+++ github/config.js
@@ -1,6 +1,6 @@
 var config = {
   githubApi : 'https://api.github.com/users/',
-  accessToken : '?access_token=3ab8d5ad38617a2135542f29472b9b80b371a688',
+  accessToken : '?access_token=1b3cb85e9aa3dc7c51934062bf8033291b93cfb4',
   codeWarsApi : 'https://www.codewars.com/api/v1/users/',
   codeWarsToken : '?access_key=s9zbYSGah3Jmy6ofmr6pC'
 };

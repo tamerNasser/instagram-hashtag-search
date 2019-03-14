4a5dc2767de7dbbec1d0283b8ff5a4b5846eccda
Tamer Nasser <tamernasser@outlook.com>
2019-03-14 12:05:00 +0200

    Removed duplicated getData func its in one script now

--- instagram/apigram.js
+++ instagram/apigram.js
@@ -4,29 +4,15 @@ const INSTAGRAM_POST_URL = 'https://www.instagram.com/p/';
 
 setPostsInfo('winter'); // Get the posts for winter as default.
 
-// Static function to get API response.
-function getData(url, cb) {
-  fetch(url)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      return cb(data);
-    })
-    .catch(function(error) {
-      return error;
-    })
-}
 
 // A function to link the search button with the input field.(add listener).
 function searchLink() {
-  let searchOption = 'Top';
-  let optionS = document.getElementById("option");
+  let optionMedia = "top";
   let input = document.getElementById("inputHashtag");
   let btnSearch = document.getElementById("btnSearchHashtag");
+  let selectOption = document.getElementById("option");
   btnSearch.addEventListener("click", function() {
-    searchOption = optionS.value;
-    setPostsInfo(input.value,searchOption);
+      setPostsInfo(input.value,selectOption.value);
   });
 }
 
@@ -34,14 +20,15 @@ function searchLink() {
 function setPostsInfo(hashtag,option) {
   let arrItems = Array.from(document.getElementsByClassName("postItem"));
   getData(INSTAGRAM_API + hashtag + INSTAGRAM_API_QUERY, function(data) {
-    let arrPosts = data.graphql.hashtag.edge_hashtag_to_top_posts.edges;
-    if(option=='Recent'){
-    arrPosts = data.graphql.hashtag.edge_hashtag_top_posts.edges;
+    let arrPosts;
+    if(option=="Top"){
+    arrPosts = data.graphql.hashtag.edge_hashtag_to_top_posts.edges;
+  }else {
+    arrPosts =  data.graphql.hashtag.edge_hashtag_to_media.edges;
   }
     for (let i = 0; i < arrItems.length; i++) {
       arrItems[i].childNodes[1].firstChild.src = arrPosts[i].node.display_url;
       arrItems[i].childNodes[1].href = INSTAGRAM_POST_URL + arrPosts[i].node.shortcode;
-      // console.log(arrPosts[i].node.edge_media_preview_like.count);
       arrItems[i].childNodes[3].innerHTML ="<img src='instagram/assets/icons/heart.png' alt='heartIcon'>"+ " " + arrPosts[i].node.edge_media_preview_like.count.toString();
       arrItems[i].childNodes[5].innerHTML = arrPosts[i].node.edge_media_to_caption.edges[0].node.text;
     }
